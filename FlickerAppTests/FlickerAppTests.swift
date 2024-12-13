a4eff067a2c3a1f69b941efd18de511472e473a0
Anusha Raju <anusha.raju0070@gmail.com>
2024-12-12 18:58:19 -0800

    Initial Commit

---
+++ FlickerAppTests/FlickerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlickerAppTests.swift
+//  FlickerAppTests
+//
+//  Created by Anusha Raju on 12/12/24.
+//
+
+import Testing
+@testable import FlickerApp
+
+struct FlickerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

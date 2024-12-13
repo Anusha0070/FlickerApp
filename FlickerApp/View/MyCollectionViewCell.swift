46a135804cdcddc263d4a5bb74e0d6c6c5385afa
Anusha Raju <anusha.raju0070@gmail.com>
2024-12-13 00:52:46 -0800

    Added search functionality to search through the images and display it on new page.

---
+++ FlickerApp/View/MyCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyCollectionViewCell.swift
+//  FlickerApp
+//
+//  Created by Anusha Raju on 12/12/24.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
